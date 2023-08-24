ef066b737ea7e718db8bdee63572ef895fddf0ed
Akari_m <58370835+Akari-my@users.noreply.github.com>
2023-08-24 21:53:50 +0200

    Update AkariSeeCommand.php

--- src/Akari/commands/AkariSeeCommand.php
+++ src/Akari/commands/AkariSeeCommand.php
@@ -3,7 +3,6 @@
 namespace Akari\commands;
 
 use Akari\Main;
-use muqsit\invmenu\inventory\InvMenuInventory;
 use muqsit\invmenu\InvMenu;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
@@ -14,45 +13,53 @@ class AkariSeeCommand extends Command {
 
     protected Main $plugin;
 
-    public function __construct(Main $plugin){
+    public function __construct(Main $plugin) {
         $this->plugin = $plugin;
         parent::__construct("akarisee", "AkariSee Command");
         $this->setPermission("akarisee.use");
     }
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool
-    {
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         $config = new Config($this->plugin->getDataFolder() . "config.yml", Config::YAML);
-        if ($sender instanceof Player) {
-            if ($sender->hasPermission("akarisee.use")) {
-                if (count($args) < 1) {
-                    $sender->sendMessage($config->get("prefix") . $config->get("command_usage"));
-                    return true;
-                }
-                $target = $this->plugin->getServer()->getPlayer($args[0]);
-                if ($target === null) {
-                    $sender->sendMessage($config->get("prefix") . $config->get("player_not_found"));
-                    return true;
-                }
-                $this->openInventory($sender, $target);
-            } else {
-                $sender->sendMessage($config->get("prefix") . $config->get("player_not_permession"));
-            }
-        } else {
+
+        if (!$sender instanceof Player) {
             $sender->sendMessage($config->get("prefix") . $config->get("command_console"));
+            return true;
         }
+
+        if (!$sender->hasPermission("akarisee.use")) {
+            $sender->sendMessage($config->get("prefix") . $config->get("player_not_permission"));
+            return true;
+        }
+
+        if (count($args) < 1) {
+            $sender->sendMessage($config->get("prefix") . $config->get("command_usage"));
+            return true;
+        }
+
+        $target = $this->plugin->getServer()->getPlayer($args[0]);
+
+        if ($target === null) {
+            $sender->sendMessage($config->get("prefix") . $config->get("player_not_found"));
+            return true;
+        }
+
+        $this->openInventory($sender, $target);
+
         return true;
     }
 
     public function openInventory(Player $viewer, Player $target): void {
+        $config = new Config($this->plugin->getDataFolder() . "config.yml", Config::YAML);
         $menu = InvMenu::create(InvMenu::TYPE_DOUBLE_CHEST)
             ->setName("Inventory of §c" . $target->getName())
-            ->setListener(function (\muqsit\invmenu\transaction\InvMenuTransaction $transaction): \muqsit\invmenu\transaction\InvMenuTransactionResult {
+            ->setListener(function (\muqsit\invmenu\transaction\InvMenuTransaction $transaction) {
                 return $transaction->discard();
-            })#
-            ->setInventoryCloseListener(function (Player $viewer, InvMenuInventory $inventory) use ($target): void {
-                $config = new Config($this->plugin->getDataFolder() . "config.yml", Config::YAML);
+            })
+            ->setInventoryCloseListener(function (Player $viewer, \muqsit\invmenu\inventory\InvMenuInventory $inventory) use ($target, $config) {
                 $viewer->sendMessage($config->get("prefix") . $config->get("player_inventory_closed") . $target->getName());
             });
+
         $menu->getInventory()->setContents($target->getInventory()->getContents());
         $menu->send($viewer);
     }
